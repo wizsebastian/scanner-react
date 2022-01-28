ead2b55a377ee5d341fa777594cde9194dec4287
Luis Vasquez <30602364+wizsebastian@users.noreply.github.com>
2022-01-28 02:31:35 -0400

    WIP

--- src/App.js
+++ src/App.js
@@ -1,14 +1,34 @@
 import ScannerComponent from './Components/Scanner'
+import { Card, Container, Row, Col } from 'react-bootstrap'
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      Escanea tu token 
-      
-      <ScannerComponent/>
-      Test by GMDTECH
-    </div>
+    <Container>
+      <Row>
+        <Col>
+          <Card style={{ width: '18rem' }}>
+            <Card.Img variant="top" src="holder.js/100px180" />
+            <ScannerComponent />
+            <Card.Body>
+              <Card.Title>
+                <h1>Presenta tu QR en pantalla</h1>
+              </Card.Title>
+              <Card.Text>
+               Cuando se haga el scan se enviara el website escaneado
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+
+    // <div >
+    //   Test by GMDTECH
+
+    // </div>
   );
 }
 
--- src/Components/Scanner/index.js
+++ src/Components/Scanner/index.js
@@ -2,28 +2,39 @@ import React from "react";
 import Scanner from "react-webcam-qr-scanner";
 
 const ScannerComponent = (props) => {
-
+const scanner_result = 'GO'
   const handleDecode = (result) => {
-    console.log(result);
+    // alert('Tu QR fue: ', result.data);
+    console.log(result.data);
+    // alert("Tu QR: ", result.data)
+    window.location.href = result.data;
+
+    // scanner_result = result.data
+//  scanner_result = window.localStorage.setItem('currentQR', JSON.stringify(result.data))
   } 
+//   scanner_result = String(handleDecode);
 
   const handleScannerLoad = (mode) => {
-    console.log(mode);
+    console.log( mode);
+    
   }
 
   return (
-    <Scanner 
-      className="some-classname"
-      onDecode={handleDecode}
-      onScannerLoad={handleScannerLoad}
-      constraints={{ 
-        audio: false, 
-        video: { 
-          facingMode: "environment" 
-        } 
-      }}
-      captureSize={{ width: 300, height: 300 }}
-    />
+          <Scanner 
+            className="some-classname"
+            onDecode={handleDecode}
+            onScannerLoad={handleScannerLoad}
+            constraints={{ 
+              audio: false, 
+              video: { 
+                facingMode: "environment" 
+              } 
+            }}
+            captureSize={{ width: 200, height: 200 }}
+          />
+
+
+
   );
 }
 
