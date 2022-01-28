4d751e7a928119b1b76b23ea18400e8239de889e
Luis Vasquez <30602364+wizsebastian@users.noreply.github.com>
2022-01-28 02:45:25 -0400

    refactor to get all sizes

--- src/Components/Scanner/index.js
+++ src/Components/Scanner/index.js
@@ -2,34 +2,38 @@ import React from "react";
 import Scanner from "react-webcam-qr-scanner";
 
 const ScannerComponent = (props) => {
-  const handleDecode = (result) => {
-    // alert('Tu QR fue: ', result.data);
-    console.log(result.data);
-    // alert("Tu QR: ", result.data)
+//   const handleDecode = (result) => {
+//     // alert('Tu QR fue: ', result.data);
+//     // alert("Tu QR: ", result.data)\
+//     // alert("Escaneado con exito!!!")
+
+//     // scanner_result = result.data
+//     //  scanner_result = window.localStorage.setItem('currentQR', JSON.stringify(result.data))
+// } 
+// //   scanner_result = String(handleDecode);
+
+const decode = (result) => {
+    console.log(result);
     window.location.href = result.data;
-
-    // scanner_result = result.data
-//  scanner_result = window.localStorage.setItem('currentQR', JSON.stringify(result.data))
-  } 
-//   scanner_result = String(handleDecode);
-
-  const handleScannerLoad = (mode) => {
+}
+const scannerLoader  = (mode) => {
     console.log( mode);
-    
-  }
+}
+
+
 
   return (
           <Scanner 
             className="some-classname"
-            onDecode={handleDecode}
-            onScannerLoad={handleScannerLoad}
+            onDecode={decode}
+            onScannerLoad={scannerLoader}
             constraints={{ 
               audio: false, 
               video: { 
                 facingMode: "environment" 
               } 
             }}
-            captureSize={{ width: 200, height: 200 }}
+            // captureSize={{ width: 200, height: 200 }}
           />
 
 
